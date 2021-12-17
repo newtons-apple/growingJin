16fe17147e22a4af2e48c2a3e56fbbd130f26b03
newtons-apple <jwc2094@naver.com>
2021-12-18 03:04:43 +0900

    move 완료

--- src/controller/move.js
+++ src/controller/move.js
@@ -2,9 +2,93 @@
  * @author g1
  * @param 
  */
+ const {monsterManager, mapManager, itemManager} = require('../datas/Manager');
+const {Item,Player,User} = require('../models')
 
-const move = async (req)=>{
+const move = async (req,res)=>{
+    const player = req.player;
+    const direction = parseInt(req.body.direction, 0); // 0 동. 1 서 . 2 남. 3 북.
+    let x = player.x; //x는 +북,-남 방향
+    let y = player.y; //y는 +동,-서 방향
+    try{
+        let canGo = mapManager.getField(x,y).canGo
+        if(!canGo[direction]){
+            player.log = '이동하지 못했다. 보이지 않는 벽이 나를 막은 듯 하다. 다른 길로 가볼까?'
+            await player.save()
+            return res.json(Player)
+        }
+        if (direction === 0) {
+          y += 1;
+        } else if (direction === 1) {
+          y -= 1;
+        } else if (direction === 2) {
+          x -= 1;
+        } else if (direction === 3) {
+          x += 1;
+        } else {
+          res.sendStatus(400);
+        }
+    
+        const field = mapManager.getField(x, y);
+        if (!field) res.sendStatus(400);
+        player.x = x;
+        player.y = y;
+    
+        const events = field.events;
+        if(events.length > 0) {
+            // Done : 확률별로 이벤트 발생하도록 변경
+            const randomNum = Math.random()*100
+            const _event = events.reduce((p,c)=>{
+                if(typeof p ==='object'){
+                    return p
+                }
+                if((p+c.percent)>randomNum){
+                    return c
+                }else{
+                    p+=c.percent
+                    return p
+                }
+            },0)
+            if(randomNum<20){
+                const events = JSON.parse(fs.readFileSync(__dirname + "../datas/events.json"))
+                const event = events.filter((event,id)=>player.level===(event.id+1))
+                const log = `${event.name}\n큰 충격을 받았다. hp 절반이 줄어들었다.`
+                player.Hp = Math.floor(player.Hp/2)
+                player.state = {...player.state,log}
+                await player.save()
+                return res.json(player)
+            }
+            if (typeof _event ==='number'){
+                const randomMent = ['다행이다','무료하다','훗'][Math.floor(Math.random()*3)] 
+                const log = '아무일도 일어나지 않았다.'+randomMent
+                player.state = {...player.state,log}
+                await player.save()
+              return res.json(player)
+            }
+            if (_event.type === "battle") {
+              // TODO: 이벤트 별로 events.json 에서 불러와 이벤트 처리
+              //enemy.remainHp 입력
+              const moster = monsterManager.getMonster(_event.monster)
+              const log = `${monster.des}\n${monster.name}:"${monster.talk}"`
+    
+              player.state = {isFighting:false,enemy:{id:monster.id,reaminHp:monster.hp},log}
+              await player.save()
+              return res.json(player)
+          }
+          if(_event.type==='item'){
+              const item = itemManager.getItem(_event.item)
+              const log = `${item.name}을(를) 획득했다. ${item.type}이 ${item.buff}만큼 증가했다..!`
+              player.state = {...player.state,log}
+              await player.save()
+              return res.json(player)
+          }
 
+        }
+    }catch(e){
+        console.error(e)
+    }
+        
+res.status(400).json({error:'something wrong'})
 }
 
 export default move
