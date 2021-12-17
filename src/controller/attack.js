583403c305e1fc96d1a17fad8d543baebb0624af
sgeunhi <alsrb582@snu.ac.kr>
2021-12-18 01:24:19 +0900

    modify attack.js

--- src/controller/attack.js
+++ src/controller/attack.js
@@ -4,14 +4,14 @@ const attack = async (req, res) => {
     const player = req.player;
     player.state.isFighting = true;
     player.state.log = "";
-    let turn = 0; // turn count
-    // TODO: turn 체크
+
     let probability = 0; // 무작위 확률로 공격 성공 or 실패
     let damage = 0; // 공격 주고 받는 데미지 -> 각각의 방어력에서 공격력을 뺀 값
     const monster = monsterManager.getMonster(player.state.enemy.id)
 
-    if (player.HP < player.maxHp * 0.2 || turn >= 10) {
+    if (player.HP < player.maxHp * 0.2 || player.state.turn >= 10) {
         probability = Math.random();
+        player.state.turn++; // turn +1
         if (probability > 0.5) {
             damage = player.str - monster.def;
             player.state.enemy.remainHp -= damage;
@@ -21,8 +21,24 @@ const attack = async (req, res) => {
                 player.HP += player.HP * 0.05;  // 남은 체력의 5% 회복
                 player.isFighting = false; // 전투 종료
                 player.exp += monster.exp;  // 경험치 획득
-                if (player.level * 10 < player.exp)
+                if (player.level * 10 < player.exp) {
                     player.level += 1;  // 레벨업
+                    player.state.log += '레벨업하였습니다. \n 다음 스테이지로 이동합니다.\n'
+                    if (player.level === 1) {
+                        player.x = 0;
+                        player.y = 0;
+                    } else if (player.level === 2) {
+                        player.x = 1;
+                        player.y = 0;
+                    } else if (player.level === 3) {
+                        player.x = 3;
+                        player.y = 0;
+                    } else if (player.level === 4) {
+                        player.x = 6;
+                        player.y = 0;
+                    }
+                }
+                player.state.turn = 0; // 전투 종료 후 turn 리셋
                 // level = {1, 2, 3, 4} => maxExp = {10, 20, 30, 40}
             }
         } else {
@@ -33,6 +49,7 @@ const attack = async (req, res) => {
                 player.state.log += '죽어버렸다...\n';
                 player.state.isFighting = false; // 전투 종료
                 player.HP = player.maxHp / 2;   // maxHP 절반의 체력으로 초기화
+                player.state.turn = 0; // 전투 종료 후 turn 리셋
                 if (player.level === 1) {
                     player.x = 0;
                     player.y = 0;
@@ -42,7 +59,7 @@ const attack = async (req, res) => {
                 } else if (player.level === 3) {
                     player.x = 3;
                     player.y = 0;
-                } else if (player.level === 2) {
+                } else if (player.level === 4) {
                     player.x = 6;
                     player.y = 0;
                 }
@@ -52,8 +69,9 @@ const attack = async (req, res) => {
     }
     // 자동 공격 이후 공격 누른 경우
 
-    while (player.HP >= player.maxHp * 0.2 && turn < 10) {
+    while (player.HP >= player.maxHp * 0.2 && player.state.turn < 10) {
         probability = Math.random();
+        player.state.turn++; // turn + 1;
         if (probability > 0.5) {
             damage = player.str - monster.def;
             player.state.enemy.remainHp -= damage;
@@ -63,14 +81,31 @@ const attack = async (req, res) => {
                 player.HP += player.HP * 0.05;  // 남은 체력의 5% 회복
                 player.isFighting = false; // 전투 종료
                 player.exp += monster.exp;  // 경험치 획득
-                if (player.level * 10 < player.exp)
+                if (player.level * 10 < player.exp) {
                     player.level += 1;  // 레벨업
+                    player.state.log += '레벨업하였습니다. \n 다음 스테이지로 이동합니다.\n'
+                    if (player.level === 1) {
+                        player.x = 0;
+                        player.y = 0;
+                    } else if (player.level === 2) {
+                        player.x = 1;
+                        player.y = 0;
+                    } else if (player.level === 3) {
+                        player.x = 3;
+                        player.y = 0;
+                    } else if (player.level === 4) {
+                        player.x = 6;
+                        player.y = 0;
+                    }
+                }
+                player.state.turn = 0; // 전투 종료 후 turn 리셋
                 // level = {1, 2, 3, 4} => maxExp = {10, 20, 30, 40}
             }
         } else {
             damage = monster.str - player.def;
             player.HP -= damage;
             player.state.log += `공격에 실패했다. ${damage}의 피해를 입었다.\n`;
+            player.state
         }
     }
     // 자동 공격 로직
