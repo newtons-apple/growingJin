813c24b4fed40d11f730fda60ba2c9859918d53b
ytwhite <mrwhite97@snu.ac.kr>
2021-12-18 18:42:59 +0900

    reset, run, ending & player stat

--- src/controller/ending.js
+++ src/controller/ending.js
@@ -1,15 +1,20 @@
 const {Item,Player,User} = require('../models')
 
-
 const ending = async (req, res) => {
+    const player = req.player;
     while (player.level === 4) {
         if (player.exp >= 60) {
             //유니콘
         } else if (player.exp >= 50) {
             //실버타운
         } else if (player.exp > 40) {
-            //과로사
+            //과로사 (사망은 좀 무례하다면.. 응급실행..?)
+            
         }
     }
 
+}
+
+module.exports = {
+    ending
 }
