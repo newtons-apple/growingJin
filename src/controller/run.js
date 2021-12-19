d921682679b39bff676c046c92d9032a843e0074
Minhong <42000516+ytwhite@users.noreply.github.com>
2021-12-19 22:33:03 +0900

    Update run.js

--- src/controller/run.js
+++ src/controller/run.js
@@ -3,7 +3,7 @@ const {monsterManager, mapManager, itemManager} = require('../datas/Manager');
 
 const run = async (req, res) => {
     const player = req.player;
-    const monster = monsterManager.getMonster(_event.monster)
+    const monster = monsterManager.getMonster(player.state.enemy.id)
     const log = `후.. ${monster.name}으로부터 도망쳤다`
         //status : normal로 변경
     player.state = {status:1,log}
